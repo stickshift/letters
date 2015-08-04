6c9e57e166a6cbd6f5ec4d752e61991f5a693be7
Andrew Young <joven@alum.wpi.edu>
2015-08-03 23:35:26 -0400

    #4 Added you got it and try again buttons to TeachViewController

--- Letters/TestViewController.m
+++ Letters/TestViewController.m
@@ -10,12 +10,27 @@
 
 @interface TestViewController()
 
+@property BOOL guessing;
+
 @property (weak, nonatomic) IBOutlet UISegmentedControl* modeSelector;
 @property (weak, nonatomic) IBOutlet UIView* chalkboardView;
+
+@property (weak, nonatomic) IBOutlet UIButton* eraseButton;
+@property (weak, nonatomic) IBOutlet UIButton* submitButton;
+@property (weak, nonatomic) IBOutlet UIButton* youGotItButton;
+@property (weak, nonatomic) IBOutlet UIButton* tryAgainButton;
 
 @end
 
 @implementation TestViewController
+
+- (void) viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.youGotItButton.hidden = YES;
+    self.tryAgainButton.hidden = YES;
+}
 
 - (void) viewWillAppear:(BOOL)animated
 {
@@ -37,6 +52,45 @@
     // Animate the drawing out of the way
     [UIView animateWithDuration:0.5 animations:^{
         self.imageView.frame = self.chalkboardView.frame;
+        
+        // Swap buttons
+        self.eraseButton.hidden = YES;
+        self.submitButton.hidden = YES;
+        self.youGotItButton.hidden = NO;
+        self.tryAgainButton.hidden = NO;
+    }];
+    
+    self.guessing = YES;
+}
+
+/**
+ * Disables drawing when guessing is true
+ */
+- (void) touchesMoved:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    if (self.guessing)
+    {
+        return;
+    }
+    
+    [super touchesMoved:touches withEvent:event];
+}
+
+- (IBAction) tryAgain:(id)sender
+{
+}
+
+- (IBAction) youGotIt:(id)sender
+{
+    self.guessing = NO;
+    [self erase:self];
+    
+    [UIView animateWithDuration:0.5 animations:^{
+        // Swap buttons
+        self.eraseButton.hidden = NO;
+        self.submitButton.hidden = NO;
+        self.youGotItButton.hidden = YES;
+        self.tryAgainButton.hidden = YES;
     }];
 }
 
